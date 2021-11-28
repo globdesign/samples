317743e3a45880c71986ed11a770f4ceae3d510d
Glob Design, LLC <globdesign@users.noreply.github.com>
2021-11-28 11:15:35 -0700

    Add files via upload

---
+++ Nicola-Sample/Nicola-Sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nicola-Sample
+//
+//  Created by i7 on 11/28/21.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+
+@end
+
